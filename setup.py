af17874c46a8b42a13c169b9eddb78df7d050094
Gökhan Yigit <goegges@gmail.com>
2019-05-28 16:38:24 +0200

    added package requirements file and setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pylana',
+    url='https://github.com/lanalabs/pylana',
+    author='Lana Labs GmbH',
+    author_email='support@lanalabs.com',
+    packages=['pylana'],
+    install_requires=[],
+    version='0.1',
+    license='MIT',
+    description='A python package to wrap log-related API calls on the Lana Process Mining Software.'
+)
