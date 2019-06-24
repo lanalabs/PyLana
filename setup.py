45cbfba5466020b17d5add5361a679322d2be6f8
jklaan <jonas.kromolan@gmail.com>
2019-06-24 16:04:58 +0200

    added methods for changing shinyurl and zipping dashboard

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pylana",
+    version="0.0.1",
+    author="Lana Labs GmbH",
+    description="Python interface for Lana API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lanalabs/pylana",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
