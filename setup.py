ebfc2b00471b962e18a74c3f0a51716b9b40497c
jklaan <jonas.kromolan@gmail.com>
2019-07-08 16:34:46 +0200

    update

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/lanalabs/pylana",
     packages=setuptools.find_packages(),
-    install_requires=['pandas', 'docopt', 'zipfile'],
+    install_requires=[],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
