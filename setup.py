f3fa3d565dffadde5efc32364ad459706e56aba6
jklaan <jonas.kromolan@gmail.com>
2019-07-15 09:40:10 +0200

    small fixes

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setuptools.setup(
     description="Python interface for Lana API",
     long_description_content_type="text/markdown",
     url="https://github.com/lanalabs/pylana.git@pylana_dist",
-    install_requires=['pylana', 'docopt', 'pandas'],
+    packages=['pylana'],
+    install_requires=['docopt', 'pandas'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
