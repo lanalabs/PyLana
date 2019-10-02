59f4149cb6b6aa06791da4b8b4ee56011d36d4f0
KevinYang <kevin@lanalabs.com>
2019-10-02 11:16:28 +0200

    fix EventLog upload

--- pylana.py
+++ pylana.py
@@ -11,10 +11,15 @@ class LanaAPI:
         self.token = token
         self.url = url
 
+        # add schema check
+        if not url.startswith('http://') or not url.startswith('https://'):
+            self.url = 'http://' + self.url
+        if not url.endswith('/'):
+            self.url += '/'
+
         self.headers = {"Authorization": self.token}
 
         userInfoEndpoint = 'api/users/by-token'
-
         self.userInfo = requests.get(url=self.url + userInfoEndpoint, headers=self.headers, verify=False).json()
 
 
@@ -37,20 +42,46 @@ class LanaAPI:
 
     def uploadEventLogWithCaseAttributes(self, logFile, logSemantics,
                                          caseAttributeFile, caseAttributeSemantics):
-        endpoint = 'api/uploadEventLogWithCaseAttributes'
+
+        # import requests
+
+        # url = "http://localhost:4000/api/logs/csv-case-attributes-event-semantics"
+
+        # payload = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"eventCSVFile\"; filename=\"daimler-FWS-current.csv\"\r\nContent-Type: text/csv\r\n\r\n\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"logName\"\r\n\r\nIncident Management\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"eventSemantics\"\r\n\r\n\n [\n   \n {\n    \"name\": \"CaseId\",\n    \"semantic\": \"Case ID\",\n    \"format\": null\n },\n  {\n    \"name\": \"Action\",\n    \"semantic\": \"Action\",\n    \"format\": null\n  },\n  {\n    \"name\": \"Start\",\n    \"semantic\": \"Start\",\n    \"format\": \"yyyy-MM-dd HH:mm:ss.SSS\"\n  }\n , {\n    \"name\": \"Complete\",\n    \"semantic\": \"Complete\",\n    \"format\": \"yyyy-MM-dd HH:mm:ss.SSS\"\n }\n   ,\n {\n    \"name\": \"duration\",\n    \"semantic\": \"NumericAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"PO\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"execstate local\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"execstate maxretry\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"execstate nested\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"execstate reason\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"result local\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"result nested\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"tgerror 1 errorid\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"tgerror 1 errortext\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"tgerror 1 flag\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"tgerror 1 trblcode\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"tgerror 2 errorid\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"tgerror 2 errortext\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"tgerror 2 flag\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"tgerror 2 trblcode\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"tgname\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"usererror 1 ecunamenisp\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"usererror 1 errorid\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"usererror 1 errortext\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"usererror 1 flag\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"usererror 1 trblcode\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"usererror 2 ecunamenisp\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"usererror 2 errorid\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"usererror 2 errortext\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"usererror 2 flag\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"usererror 2 trblcode\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }\n  ]\n     \r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"caseAttributeFile\"; filename=\"daimler-FWS-current-caseAttributes.csv\"\r\nContent-Type: text/csv\r\n\r\n\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"caseSemantics\"\r\n\r\n\n [\n   {\n     \"name\": \"CaseId\",\n     \"semantic\": \"Case ID\",\n     \"format\": null\n   }\n   ,\n {\n    \"name\": \"Fehleranzahl\",\n    \"semantic\": \"NumericAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"Antrieb\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"Aufbauvariante\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"AuftrittsPO\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"Baumuster\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"Baureihe\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"Fehlertyp\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"Fehlerursache\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"Getriebetyp\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"Kraftstoff\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"Lenkervariante\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"Motortyp\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"Zylinder\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"all_testgroups_IO\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"erfassungszeit_s_dat_u_zeit\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"erfassungszeit_u_dat_u_zeit\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"fehlerart_u\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"fehlerort_u\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"folgefehler\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"is_fehlercode\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"is_fehlertext\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"na_kzn\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"prodno\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }, \n {\n    \"name\": \"vin\",\n    \"semantic\": \"CategorialAttribute\",\n    \"format\": null\n }\n ]\n     \r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"timeZone\"\r\n\r\nEurope/Berlin\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"
+        # headers = {
+        #     'content-type': "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
+        #     'Content-Type': "application/x-www-form-urlencoded",
+        #     'Authorization': "API-Key 3e18d628a34a4afaba3058af34275ccc",
+        #     'User-Agent': "PostmanRuntime/7.16.3",
+        #     'Accept': "*/*",
+        #     'Cache-Control': "no-cache",
+        #     'Postman-Token': "f83270c8-060d-47e4-94f4-800948f00b67,19cc30e8-31a1-47a9-9242-a4e38cd11d1b",
+        #     'Host': "localhost:4000",
+        #     'Accept-Encoding': "gzip, deflate",
+        #     'Content-Length': "591868",
+        #     'Connection': "keep-alive",
+        #     'cache-control': "no-cache"
+        #     }
+
+        # response = requests.request("POST", url, data=payload, headers=headers)
+        endpoint = 'api/logs/csv-case-attributes-event-semantics'
 
         files = {
-            'eventCSVFile': open(logFile, 'rb'),
-            'caseAttributeFile': open(caseAttributeFile, 'rb'),
+            'eventCSVFile': (logFile.split('/')[-1], open(logFile, 'rb'), 'text/csv'),
+            'caseAttributeFile': (caseAttributeFile.split('/')[-1], open(caseAttributeFile, 'rb'), 'text/csv'),
         }
 
         semantics = {
             'eventSemantics': open(logSemantics).read(),
-            'caseSemantics': open(caseAttributeSemantics).read()
+            'caseSemantics': open(caseAttributeSemantics).read(),
+            'logName': "Incident Management",
+            'timeZone': "Europe/Berlin"
         }
 
-        upload_response = requests.post(self.url + endpoint, headers=self.headers, files=files,
-                                        data=semantics, verify=False).json()
+        new_header = dict(self.headers)
+        upload_response = requests.request('POST', self.url + endpoint, headers=new_header, files=files, data=semantics)
+        print(requests.Request('POST', self.url + endpoint, headers=new_header, files=files, data=semantics).prepare().body.decode('ascii'))
+        
         return upload_response
 
     def getUserLogs(self):
@@ -157,11 +188,13 @@ class LanaAPI:
     def uploadShinyDashboard(self, dashboard, dashboardId):
         uploadShinyEndpoint = "api/shiny-dashboards/" + str(dashboardId) + "/source"
         file = {'file': open(dashboard, 'rb')}
-        requests.post(self.url + uploadShinyEndpoint, headers=self.headers, files=file, verify=False)
+        res = requests.post(self.url + uploadShinyEndpoint, headers=self.headers, files=file, verify=False)
+        return res
 
     def connectDashboardToLog(self, dashboardId, logId):
         connectDashboardToLogEndpoint = "api/logs/" + str(logId) + "/shiny-dashboard/" + str(dashboardId)
-        requests.post(self.url + connectDashboardToLogEndpoint, headers=self.headers, verify=False)
+        res = requests.post(self.url + connectDashboardToLogEndpoint, headers=self.headers, verify=False)
+        return res
 
     # Id arguments need to be lists
     def shareDashboard(self, dashboardId, userIds, projectIds, organizationIds):
