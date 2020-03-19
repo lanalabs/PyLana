5d98565d0e1cfe75d6c79283c5674db615f1a481
Peter Drechsel <peter@lanalabs.com>
2020-03-19 15:44:50 +0100

    Improve handling of failing requests

--- pylana/utils.py
+++ pylana/utils.py
@@ -2,6 +2,8 @@ import functools
 import json
 from typing import List
 
+import requests
+
 
 def _create_headers(token: str):
     return {"Authorization": f"API-Key {token}"}
@@ -20,10 +22,14 @@ def expect_json(func):
     return parse_json
 
 
+# TODO: introduce proper logging
 def handle_response(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         resp = func(*args, **kwargs)
-        resp.raise_for_status()
+        try:
+            resp.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            print(e)
         return resp
     return wrapper
