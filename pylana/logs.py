81e91b969b1c02e8c8da3cebde22c2898bdb1bd7
kimon <kimon@lana-labs.com>
2020-05-05 15:57:59 +0200

    Discard string encoding of event log response

--- pylana/logs.py
+++ pylana/logs.py
@@ -196,7 +196,7 @@ class LogsAPI(ResourceAPI):
 
         log_id = log_id or self.get_log_id(log_name)
         resp = self.request_event_csv(log_id)
-        csv_stream = io.StringIO(resp.text)
+        csv_stream = io.BytesIO(resp.content)
         return pd.read_csv(csv_stream, dtype='object', **kwargs)
 
     @handle_response
