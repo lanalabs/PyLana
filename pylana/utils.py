e20a3ce9d84c5d0a1b70039b6890958f959e1fb3
Peter Drechsel <peter@lanalabs.com>
2020-10-08 14:42:44 +0200

    Add descriptive and impact attributes to semantics inference

--- pylana/utils.py
+++ pylana/utils.py
@@ -2,41 +2,63 @@
 functions to prepare requests for consumption
 """
 
-from collections import defaultdict
-from typing import Iterable, Dict, List, Union
+from typing import Iterable, Dict, List, Optional
 
-import re
 import pandas as pd
 
 
-# TODO: check whether this function is actually required
+def _try_to_map(key, value):
+    return {key: value} if key is not None else dict()
+
+
 def create_semantics(columns: Iterable[str],
-                     case_id: str = 'id', action: str = 'action', start: str = 'start', complete: str = 'complete',
+                     case_id: str = 'id', action: Optional[str] = 'action',
+                     start: Optional[str] = 'start', complete: Optional[str] =
+                     'complete',
                      numerical_attributes: Iterable[str] = tuple(),
-                     impact_attributes : Iterable[str] = tuple(),
+                     impact_attributes: Iterable[str] = tuple(),
                      descriptive_attributes: Iterable[str] = tuple(),
                      time_format: str = 'yyyy-MM-dd HH:mm:ss') -> List[Dict[str, str]]:
     """
     create semantics including numeric and categorical attributes
 
     Args:
-        columns: strings representing the columns  of the table
-        case_id: the column name for the case ids
-        action: the column name for the activities
-        start: the column name for the start timestamp
-        complete: the column name for the complete timestamp
-        numerical_attributes: the column names for numerical attributes
-        impact_attributes: the column names for impact attributes
-        descriptive_attributes: the column names for descriptive attributes
-        time_format: the time format for start and complete columns
+        columns:
+            An list of strings representing the column names of the
+            table.
+        case_id:
+            A string representing the column name for the case id.
+        action:
+            A string representing the column name for the activities
+            (optional).
+        start:
+            A string representing the column name for the start timestamp
+            (optional).
+        complete:
+            A string representing the column name for the complete timestamp
+            (optional).
+        numerical_attributes:
+            An list of strings representing the column names for numerical
+            attributes (optional), defaults to empty tuple.
+        impact_attributes:
+            An list of strings representing the column names for impact
+            attributes (optional), defaults to empty tuple.
+        descriptive_attributes:
+            An list of strings representing  the column names for
+            descriptive attributes (optional), defaults to empty tuple.
+        time_format:
+            A string representing the time format for start and complete
+            columns.
 
     Returns:
-        a list of dicts representing the semantics file
+        A list of dictionaries representing the semantics.
     """
 
-    semantics = {
-        **{case_id: 'Case ID', action: 'Action',
-           start: 'Start', complete: 'Complete'},
+    semantics_map = {
+        **{case_id: 'Case ID'},
+        **_try_to_map(action, 'Action'),
+        **_try_to_map(start, 'Start'),
+        **_try_to_map(complete, 'Complete'),
         **{att: 'NumericAttribute' for att in numerical_attributes},
         **{att: 'DescriptiveAttribute' for att in descriptive_attributes},
         **{att: 'ImpactAttribute' for att in impact_attributes}
@@ -46,106 +68,110 @@ def create_semantics(columns: Iterable[str],
         {
             'idx': idx,
             'name': col,
-            'semantic': semantics.get(col, 'CategorialAttribute'),
+            'semantic': semantics_map.get(col, 'CategorialAttribute'),
             'format': time_format if col in [start, complete] else None
         } for idx, col in enumerate(columns)
     ]
 
 
-def create_event_semantics_from_df(df: pd.DataFrame, case_id: str = 'Case_ID', action: str = 'Action',
-                                   start: str = 'Start', complete: str = 'Complete',
-                                   time_format: str = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS") -> List[dict]:
+def _is_lana_numeric(ser):
+    return \
+        pd.api.types.is_numeric_dtype(ser) and \
+        not pd.api.types.is_bool_dtype(ser)
+
+
+def create_event_semantics_from_df(
+        df: pd.DataFrame, case_id: str = 'Case_ID', action: str = 'Action',
+        start: str = 'Start', complete: str = 'Complete',
+        impact_attributes: Iterable[str] = tuple(),
+        descriptive_attributes: Iterable[str] = tuple(),
+        time_format: str = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS") -> List[dict]:
     """
-    create event semantics from a pandas data frame
+    Create event semantics from a pandas data frame.
 
-    The columns semantic for lana will be derived from the data frame's dtypes. All types
-    will be converted to categorical attributes, besides numbers. The Start and Complete
-    time stamps need to have the same time format. For an overview over time stamp formats
-    see https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html#patterns
+    The columns semantic for lana will be derived from the data frame's dtypes.
+    All types will be converted to categorical attributes, besides numbers,
+    impact and descriptive attributes. Impact and and descriptive attributes
+    are special cases of categorial attributes with teh dtype 'object'.
+    columns with this semantics can not be inferred. They have to be passed
+    to this function.
+
+    The Start and Complete time stamps need to have the same time format.
+    For an overview over time stamp formats see
+    https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html#patterns
+
+    Args:
+        df:
+            A data frame representing amn event log.
+        case_id:
+            A string representing the column name for the case id.
+        action:
+            A string representing the column name for the activity.
+        start:
+            A string representing the column name for the start timestamp.
+        complete:
+            A string representing the column name for the complete timestamp.
+        impact_attributes:
+            A list of strings representing the column names for impact
+            attributes.
+        descriptive_attributes:
+            A list of strings representing the column names for descriptive
+            attributes.
+        time_format:
+            A string representing the time format for start and complete
+            columns.
+
+    Returns:
+        A list of dictionaries representing the log semantics.
     """
-    semantics = []
-    id_mappings = {
-        case_id: 'Case ID', action: 'Action'
-    }
-    timestamps_mappings = {
-        start: 'Start', complete: 'Complete'
-    }
-
-    for i, col in enumerate(df.columns):
-        is_lana_categorial = \
-            pd.api.types.is_object_dtype(df[col]) or \
-            pd.api.types.is_bool_dtype(df[col])
-        is_lana_numeric = \
-            pd.api.types.is_numeric_dtype(df[col]) and not \
-                pd.api.types.is_bool_dtype(df[col])
-
-        if col in id_mappings:
-            # the most general branch, that exists in all logs
-            semantics += [{
-                'name': col,
-                'semantic': id_mappings[col],
-                'format': None,
-                'idx': i}]
-        elif col in timestamps_mappings:
-            # the second most general branch, Start exists always, Complete sometimes
-            semantics += [{
-                'name': col,
-                'semantic': timestamps_mappings[col],
-                'format': time_format,
-                'idx': i}]
-        else:
-            # an optional branch, defined by not being (partially) required and thus inferred
-            if is_lana_categorial:
-                semantics += [{
-                    'name': col,
-                    'semantic': 'CategorialAttribute',
-                    'format': None,
-                    'idx': i}]
-            elif is_lana_numeric:
-                semantics += [{
-                    'name': col,
-                    'semantic': 'NumericAttribute',
-                    'format': None,
-                    'idx': i}]
-
-    return semantics
+    return create_semantics(
+        df.columns,
+        case_id,
+        action,
+        start,
+        complete,
+        numerical_attributes=[col for col in df.columns
+                              if _is_lana_numeric(df.loc[:, col])],
+        impact_attributes=impact_attributes,
+        descriptive_attributes=descriptive_attributes,
+        time_format=time_format
+    )
 
 
-def create_case_semantics_from_df(df: pd.DataFrame, case_id: str = 'Case_ID') -> List[dict]:
+def create_case_semantics_from_df(
+        df: pd.DataFrame, case_id: str = 'Case_ID',
+        impact_attributes: Iterable[str] = tuple(),
+        descriptive_attributes: Iterable[str] = tuple()) -> List[dict]:
     """
-    create case semantics from a pandas data frame
+    Create event semantics from a pandas data frame.
 
-    The columns semantic for lana will be derived from the data frame's dtypes. All types
-    will be converted to categorical attributes, besides numbers.
+    The columns semantic for lana will be derived from the data frame's dtypes.
+    All types will be converted to categorical attributes, besides numbers,
+    impact and descriptive attributes. Impact and and descriptive attributes
+    are special cases of categorial attributes with teh dtype 'object'.
+    columns with this semantics can not be inferred. They have to be passed
+    to this function.
+
+    Args:
+        df:
+            A data frame representing amn event log.
+        case_id:
+            A string representing the column name for the case id.
+        impact_attributes:
+            A list of strings representing the column names for impact
+            attributes.
+        descriptive_attributes:
+            A list of strings representing the column names for descriptive
+            attributes.
+
+    Returns:
+        A list of dictionaries representing the log semantics.
     """
-
-    semantics = []
-    id_mappings = {case_id: 'Case ID'}
-    for i, col in enumerate(df.columns):
-        is_lana_categorial = \
-            pd.api.types.is_object_dtype(df[col]) or \
-            pd.api.types.is_bool_dtype(df[col])
-        is_lana_numeric = \
-            pd.api.types.is_numeric_dtype(df[col]) and not \
-                pd.api.types.is_bool_dtype(df[col])
-
-        if col in id_mappings:
-            semantics += [{
-                'name': col,
-                'semantic': id_mappings[col],
-                'format': None,
-                'idx': i}]
-        elif is_lana_categorial:
-            semantics += [{
-                'name': col,
-                'semantic': 'CategorialAttribute',
-                'format': None,
-                'idx': i}]
-        elif is_lana_numeric:
-            semantics += [{
-                'name': col,
-                'semantic': 'NumericAttribute',
-                'format': None,
-                'idx': i}]
-
-    return semantics
+    return create_semantics(
+        df.columns,
+        case_id,
+        numerical_attributes=[col for col in df.columns
+                              if _is_lana_numeric(df.loc[:, col])],
+        impact_attributes=impact_attributes,
+        descriptive_attributes=descriptive_attributes
+    )
